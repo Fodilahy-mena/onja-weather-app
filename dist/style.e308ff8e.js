3b0593ea7d312e8d8346ef47ce5a8b51aa399220
JEROME Emilien <jerome.emi@onja.org>
2021-01-19 15:40:52 +0300

    added some styles

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58684" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54809" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
